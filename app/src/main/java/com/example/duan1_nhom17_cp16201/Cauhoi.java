d4d27319897bd31ce97884e45d0cf590cb0e44bd
Admin <Hoaison2302@gmail.com>
2021-08-03 22:15:23 +0700

    update code

---
+++ app/src/main/java/com/example/duan1_nhom17_cp16201/Cauhoi.java
@@ -0,0 +1,27 @@
+package com.example.duan1_nhom17_cp16201;
+
+public class Cauhoi {
+
+    public String dapan;
+    public byte[] img;
+
+    public String getDapan() {
+        return dapan;
+    }
+
+    public void setDapan(String dapan) {
+        this.dapan = dapan;
+    }
+
+    public byte[] getImg() {
+        return img;
+    }
+
+    public void setImg(byte[] img) {
+        this.img = img;
+    }
+
+    public Cauhoi() {
+    }
+
+}
